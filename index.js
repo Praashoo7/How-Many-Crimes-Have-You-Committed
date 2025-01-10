d07910377f5cb800ebc731e88f45a49ba60847b4
Prashant <rnttl90@gmail.com>
2025-01-10 14:34:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -270,7 +270,7 @@ async function fetchUserStats() {
         chart.dispose();
     }
     try {
-        const response = await fetch(`https://github-stats-worker.gdata329947254.workers.dev/${username}`);
+        const response = await fetch(`https://github-stats-worker.gdata85416.workers.dev/${username}`);
     
         if (!response.ok) {
             if (response.status === 429) {
